32c6c8038306fd6b5ca3249fb6925c902235eddc
ArunavKishor <arunavkishor@gmail.com>
2025-11-19 17:00:16 +0530

    collecting data

--- JavaScript/RICR inquiry form/script.js
+++ JavaScript/RICR inquiry form/script.js
@@ -1,18 +1,67 @@
-const namee=document.getElementById("person").value;
-console.log("Name : "+  namee);
-const Contact=document.getElementById("number").value;
-console.log("Contact : "+  Contact );
-// ----------------------------------------------------------------------------------------------------------
-const Email=document.getElementById("email").value;
-console.log("Email : "+  Email );
-const qualification=document.getElementById("qualification").value;
-console.log("qualification : "+  qualification );
-// ------------------------------------------------------------------------------------------------------------
-const CollageName=document.getElementById("collage").value;
-console.log("Collage Name : "+  CollageName );
-// ------------------------------------------------------------------------------------------------------------
-const Year=document.getElementById("year").value;
-console.log("Year : "+  Year );
-const Branch=document.getElementById("branch").value;
-console.log("Year : "+  Branch );
-// ------------------------------------------------------------------------------------------------------------
+function Login() {
+  const namee = document.getElementById("person").value;
+  console.log("Name : " + namee);
+  const Contact = document.getElementById("number").value;
+  console.log("Contact : " + Contact);
+  // ----------------------------------------------------------------------------------------------------------
+  const Email = document.getElementById("email").value;
+  console.log("Email : " + Email);
+  const qualification = document.getElementById("qualification").value;
+  console.log("qualification : " + qualification);
+  // ------------------------------------------------------------------------------------------------------------
+  const CollageName = document.getElementById("collage").value;
+  console.log("Collage Name : " + CollageName);
+  // ------------------------------------------------------------------------------------------------------------
+  const Year1 = document.getElementById("2").value;
+  console.log("Year : " + Year1);
+  const Year2 = document.getElementById("3").value;
+  console.log("Year : " + Year2);
+  const Year3 = document.getElementById("4").value;
+  console.log("Year : " + Year3);
+  const Year4 = document.getElementById("5").value;
+  console.log("Year : " + Year4);
+  const Year5 = document.getElementById("6").value;
+  console.log("Year : " + Year5);
+  const Year6 = document.getElementById("7").value;
+  console.log("Year : " + Year6);
+  const Branch = document.getElementById("branch").value;
+  console.log("Branch : " + Branch);
+  // ------------------------------------------------------------------------------------------------------------
+const Full = document.getElementById("Full").value;
+  console.log("Cource : " + Full);
+const DataS = document.getElementById("DataS").value;
+  console.log("Cource : " + DataS);
+  const DataA = document.getElementById("DataA").value;
+  console.log("Cource : " + DataA);
+  const Business = document.getElementById("Business").value;
+  console.log("Cource : " + Business);
+  const Java = document.getElementById("Java").value;
+  console.log("Cource : " + Java);
+  const Python = document.getElementById("Python").value;
+  console.log("Cource : " + Python);
+  const C = document.getElementById("C").value;
+  console.log("Cource : " + C);
+  const Other = document.getElementById("Other").value;
+  console.log("Cource : " + Other);
+
+
+ const News = document.getElementById("News").value;
+  console.log("Source : " + News);
+ const Friends = document.getElementById("Friends").value;
+  console.log("Source : " + Friends);
+ const Social = document.getElementById("Social").value;
+  console.log("Source : " + Social);
+ const Workshop = document.getElementById("Workshop").value;
+  console.log("Source : " + Workshop);
+ const Received = document.getElementById("Received").value;
+  console.log("Source : " + Received);
+ const others = document.getElementById("others").value;
+  console.log("Source : " + others);
+
+  const Kasim = document.getElementById("Kasim").value;
+  console.log("Source : " + Kasim);
+  const Depali = document.getElementById("Depali").value;
+  console.log("Source : " + Depali);
+  const Anchal = document.getElementById("Anchal").value;
+  console.log("Source : " + Anchal);
+}
