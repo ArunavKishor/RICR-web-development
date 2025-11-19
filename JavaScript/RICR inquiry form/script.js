cdea77d34bd840f13c749e0089482e07ef658a16
ArunavKishor <arunavkishor@gmail.com>
2025-11-19 14:01:14 +0530

    enquiry form

---
+++ JavaScript/RICR inquiry form/script.js
@@ -0,0 +1,18 @@
+const namee=document.getElementById("person").value;
+console.log("Name : "+  namee);
+const Contact=document.getElementById("number").value;
+console.log("Contact : "+  Contact );
+// ----------------------------------------------------------------------------------------------------------
+const Email=document.getElementById("email").value;
+console.log("Email : "+  Email );
+const qualification=document.getElementById("qualification").value;
+console.log("qualification : "+  qualification );
+// ------------------------------------------------------------------------------------------------------------
+const CollageName=document.getElementById("collage").value;
+console.log("Collage Name : "+  CollageName );
+// ------------------------------------------------------------------------------------------------------------
+const Year=document.getElementById("year").value;
+console.log("Year : "+  Year );
+const Branch=document.getElementById("branch").value;
+console.log("Year : "+  Branch );
+// ------------------------------------------------------------------------------------------------------------
