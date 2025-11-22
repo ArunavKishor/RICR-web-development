5432932b022a19a4aebfefeea5988b5d6d6547fb
ArunavKishor <arunavkishor@gmail.com>
2025-11-22 13:49:14 +0530

    form

--- JavaScript/RegistrationForm/script.js
+++ JavaScript/RegistrationForm/script.js
@@ -1,7 +1,19 @@
 function submit(){
     
-    const batch=document.querySelectorAll("input[name='batch']:checked");
-    console.log(batch);
-
-    // const selectedBatch=batch.forEach();
+   const name = document.getElementById("fullName").value;
+    console.log(name);
+     const email = document.getElementById("email").value;
+    console.log(email);
+     const mobile = document.getElementById("mobile").value;
+    console.log(mobile);
+    const dob = document.getElementById("dob").value;
+    console.log(dob);
+//    ----------------------------------------------------------
+    const qualification = document.getElementById("qualification").value;
+    console.log(qualification);
+     const score = document.getElementById("score").value;
+    console.log(score);
+    // ----------------------------------------------------------
+    const course = document.getElementById("course").value;
+    console.log(course);
 }
