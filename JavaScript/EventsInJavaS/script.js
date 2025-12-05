a1a83806c9f864db8b10f659f2290554add0c68a
ArunavKishor <arunavkishor@gmail.com>
2025-12-05 20:30:40 +0530

    responsive

--- JavaScript/EventsInJavaS/script.js
+++ JavaScript/EventsInJavaS/script.js
@@ -34,3 +34,29 @@ function SB_control() {
 function SB_control2() {
   document.getElementById("smartBulb").classList.toggle("On");
 }
+
+document.getElementById("c1").addEventListener("mouseenter",()=>{fillColour("red")});
+document.getElementById("c2").addEventListener("mouseenter",()=>{fillColour("orange")});
+document.getElementById("c3").addEventListener("mouseenter",()=>{fillColour("yellow")});
+document.getElementById("c4").addEventListener("mouseenter",()=>{fillColour("green")});
+document.getElementById("c5").addEventListener("mouseenter",()=>{fillColour("blue")});
+document.getElementById("c6").addEventListener("mouseenter",()=>{fillColour("indigo")});
+document.getElementById("c7").addEventListener("mouseenter",()=>{fillColour("violet")});
+
+// document.getElementById("led").addEventListener("mouseleave",()=>{fillColourA("white")});
+
+// function fillColourA(Colour){
+//   document.getElementById("led").style.backgroundColor=Colour;
+// }
+
+document.getElementById("c1").addEventListener("mouseleave",()=>{fillColour("white")});
+document.getElementById("c2").addEventListener("mouseleave",()=>{fillColour("white")});
+document.getElementById("c3").addEventListener("mouseleave",()=>{fillColour("white")});
+document.getElementById("c4").addEventListener("mouseleave",()=>{fillColour("white")});
+document.getElementById("c5").addEventListener("mouseleave",()=>{fillColour("white")});
+document.getElementById("c6").addEventListener("mouseleave",()=>{fillColour("white")});
+document.getElementById("c7").addEventListener("mouseleave",()=>{fillColour("white")});
+
+function fillColour(colour){
+  document.getElementById("led").style.backgroundColor=colour;
+}
