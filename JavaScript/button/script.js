4ab27d4bb033e93eabab1d27bb41ab37fa080dc0
ArunavKishor <arunavkishor@gmail.com>
2025-12-06 19:44:16 +0530

    button practice

---
+++ JavaScript/button/script.js
@@ -0,0 +1,14 @@
+function Addbutton(){
+    // const task=document.getElementById("btn").value;
+    // const b=document.createElement("Button");
+    console.log("hi");
+    const a= document.getElementById("div");
+    const btn=document.createElement("button");
+    btn.innerHTML="Arunav";
+    a.append(btn);
+    // b.innerHTML("Button");
+    // if(task){
+    //     const b=document.createElement("button");
+    //     b.classButton.add("btn","btn-primary");
+    // }
+}
