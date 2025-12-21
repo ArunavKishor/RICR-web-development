bbec8138f739025ba89a5eeab46a37521954e7e5
ArunavKishor <arunavkishor@gmail.com>
2025-12-21 13:03:57 +0530

    some changes

--- jokesapp/script.js
+++ jokesapp/script.js
@@ -8,3 +8,11 @@ async function getnewjoke(){
     document.getElementById("setup").innerText=data.setup;
     document.getElementById("punchline").innerText=data.punchline;
 }
+async function getnewjoke1(){
+
+    const response=await fetch("https://official-joke-api.appspot.com/jokes/random");
+
+    const data=await response.json();
+    document.getElementById("setup1").innerText=data.setup;
+    document.getElementById("punchline1").innerText=data.punchline;
+}
