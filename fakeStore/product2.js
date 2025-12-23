8343c5f54c120bd2e8203625ec201d1aae3b2786
ArunavKishor <arunavkishor@gmail.com>
2025-12-23 20:30:52 +0530

    Add HTML structure and JavaScript functionality for Fake Store website
    - Created about.html, contact.html, index.html, product.html, and product2.html with Bootstrap styling and navigation.
    - Implemented product fetching and display functionality in product.js and product2.js.
    - Added CSS styles in style.css for hover effects and layout adjustments.
    - Established a consistent navigation bar across all pages.

---
+++ fakeStore/product2.js
@@ -0,0 +1,54 @@
+async function getProducts() {
+  try {
+    const res = await fetch("https://fakestoreapi.com/products");
+
+    const data = await res.json();
+
+    const productList = document.getElementById("productRow");
+
+    data.forEach((element) => {
+      const d = document.createElement("div");
+      d.classList.add("row-4");
+
+      d.innerHTML = `<div class="border border-1 w-100 rounded d-flex p-lg-3 mt-3 gap-3">
+            <div class="w-25 container border" style="width: 150px; height: 250px;">
+              <img
+                src=${element.image}
+                alt=${element.title}
+                class="object-fit-contain rounded"
+               style="width: 150px; height: 350px;"
+              />
+            </div>
+           <div>
+             <div class="h-50 p-2">
+              <div class="fw-bold fs-4">${
+                element.title.length > 50
+                  ? element.title.slice(0, 45) + "..."
+                  : element.title
+              }</div>
+              <div class="fw-semibold">${element.rating.rate}/5 (${
+        element.rating.count
+      })</div>
+              <div class="fw-semibold fs-5">₹ ${element.price * 100}</div>
+              <div class="mb-2">
+               ${element.description.slice(0, 300)}...
+              </div>
+              <div class="d-flex justify-content-start gap-3">
+                <button class="btn btn-outline-primary">Add to Cart</button>
+                <button class="btn btn-primary">Buy Now</button>
+              </div>
+            </div>
+           </div>
+          </div>
+        
+
+`;
+
+      productList.appendChild(d);
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+getProducts();
