cfa9c8139f1cdcc4cde545ac1f1373c55177666a
ArunavKishor <arunavkishor@gmail.com>
2025-12-26 19:24:22 +0530

    Add Bootstrap dependencies and apply background color to main div in App component

--- react/Devo/src/App.jsx
+++ react/Devo/src/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.min.js"
+import "bootstrap-icons/font/bootstrap-icons.css"
+
 
 function App() {
   return (
     <>
       <h1 id="head">Hellow World </h1>
-      <div>this is my first react app </div>
+      <div className="bg-primary">this is my first react app </div>
     </>
   );
 }
