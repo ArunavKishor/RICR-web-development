e01d8e6154f4091d52ca9de46e0f6c14206e1f84
ArunavKishor <arunavkishor@gmail.com>
2025-12-29 15:03:59 +0530

    changes

--- react/Makup/src/pages/Home.jsx
+++ react/Makup/src/pages/Home.jsx
@@ -11,32 +11,33 @@ import SC14 from "../assets/sc14.png";
 const Home = () => {
   return (
     <>
-      <div className="text-center ">
+      <div className="flex flex-col">
+        <div className="text-center ">
         <br />
         <span className="text-4xl font-semibold">NEW LAUNCHES</span>
         <br />
         <br />
         <div className="flex justify-between ml-7 mr-7 ">
-          <div>
-            <img src={SC1} alt="image 1" className="h-100" />
+          <div className="h-100">
+            <img src={SC1} alt="image 1"  />
             <button className="border-2 rounded w-100 font-bold text-white bg-black p-2">
               ADD TO CART
             </button>
           </div>
-          <div>
-            <img src={SC2} alt="image 2" className="h-100" />
+          <div className="h-100">
+            <img src={SC2} alt="image 2"  />
             <button className="border-2 rounded w-100 font-bold text-white bg-black p-2">
               ADD TO CART
             </button>
           </div>
-          <div>
-            <img src={SC3} alt="image 3" className="h-100" />
+          <div className="h-100">
+            <img src={SC3} alt="image 3"  />
             <button className="border-2 rounded w-100 font-bold text-white bg-black p-2">
               ADD TO CART
             </button>
           </div>
-          <div>
-            <img src={SC4} alt="image 4" className="h-100" />
+          <div className="h-100" >
+            <img src={SC4} alt="image 4" />
             <button className="border-2 rounded w-100 font-bold text-white bg-black p-2">
               ADD TO CART
             </button>
@@ -51,26 +52,26 @@ const Home = () => {
         <br />
         <br />
         <div className="flex justify-between ml-7 mr-7 ">
-          <div>
-            <img src={SC11} alt="image 1" className="h-100" />
+          <div className="h-100">
+            <img src={SC11} alt="image 1"  />
             <button className="border-2 rounded w-100 font-bold text-white bg-black p-2">
               ADD TO CART
             </button>
           </div>
-          <div>
-            <img src={SC12} alt="image 2" className="h-100" />
+          <div className="h-100">
+            <img src={SC12} alt="image 2"  />
             <button className="border-2 rounded w-100 font-bold text-white bg-black p-2">
               ADD TO CART
             </button>
           </div>
-          <div>
-            <img src={SC13} alt="image 3" className="h-100" />
+          <div className="h-100">
+            <img src={SC13} alt="image 3"  />
             <button className="border-2 rounded w-100 font-bold text-white bg-black p-2">
               ADD TO CART
             </button>
           </div>
-          <div>
-            <img src={SC14} alt="image 4" className="h-100" />
+          <div className="h-100">
+            <img src={SC14} alt="image 4"  />
             <button className="border-2 rounded w-100 font-bold text-white bg-black p-2">
               ADD TO CART
             </button>
@@ -80,8 +81,9 @@ const Home = () => {
 
       {/* ----------------------------------------------- */}
 
-      <div className="container flex mt-[70px] justify-between">
-        <button className="border-2 p-3 text-white bg-black rounded items-center">VIEW ALL</button>
+      <div className="container flex mt-\[70px]\ ">
+        <button className="border-2 p-3 text-white bg-black rounded justify-center">VIEW ALL</button>
+      </div>
       </div>
     </>
   );
