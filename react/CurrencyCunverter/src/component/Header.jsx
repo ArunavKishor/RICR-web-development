49ad06ce0dfc4e7a471b9ffaf6452ba5db92342a
ArunavKishor <arunavkishor@gmail.com>
2026-01-01 19:22:58 +0530

    new folder made

---
+++ react/CurrencyCunverter/src/component/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <><div className='bg-blue-500 px-4 py-2 text-white text-center'>
+        </div>
+        
+        </>
+  )
+}
+
+export default Header;
