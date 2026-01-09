1f78869eb9ab3452cc3cd18a9e89d7d3734eec86
ArunavKishor <arunavkishor@gmail.com>
2026-01-09 19:32:31 +0530

    Add MongoDB connection setup and update dependencies

--- node/firstBackend/server.js
+++ node/firstBackend/server.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
+import connectDB from "./src/config/db.js";
 
 const app = express();
 
@@ -12,4 +13,5 @@ app.get("/", (request, response) => {
 const port = process.env.PORT||5000;
 app.listen(port,()=>{
     console.log("Server Started at port",port);
+    connectDB();
 })
