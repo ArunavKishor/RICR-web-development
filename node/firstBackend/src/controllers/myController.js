600587e747e1028295c319505a0704fa0dda6e87
ArunavKishor <arunavkishor@gmail.com>
2026-01-10 18:54:53 +0530

    Add email existence check in user registration

--- node/firstBackend/src/controllers/myController.js
+++ node/firstBackend/src/controllers/myController.js
@@ -9,6 +9,12 @@ export const UserRegister = async (request, response) => {
       return;
     }
 
+    const existingUser= await User.findOne({email});
+    if(existingUser){
+      res.status(409).json({message:"Email Already Exists"});
+      return;
+    }
+
     const newUser = await User.create({ fullName, email, phone, password });
 
     console.log(newUser);
