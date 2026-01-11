71a43575d4ca8eef8e842e483eee3e8738d2789c
ArunavKishor <arunavkishor@gmail.com>
2026-01-11 18:46:22 +0530

    feat: enhance registration form with validation and state management

--- Craving's/client/src/App.jsx
+++ Craving's/client/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "./components/Header";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import About from "./pages/About"
+import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Login from "./pages/Login";
 import Register from "./pages/register";
