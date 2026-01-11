e7d17c03b9d37d87ac9d52e08d5257931894edc1
ArunavKishor <arunavkishor@gmail.com>
2026-01-11 16:43:33 +0530

    Fix database connection logging in db.js and enable database connection in index.js

--- Craving's/server/index.js
+++ Craving's/server/index.js
@@ -2,12 +2,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
+import conectDB from "./src/config/db.js";
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/auth", Authrouter);
+// app.use("/auth", Authrouter);
 
 app.get("/", (req, res) => {
   console.log("server is working");
@@ -16,4 +17,5 @@ app.get("/", (req, res) => {
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log("Server started at port");
+  conectDB();
 });
--- Craving's/server/src/config/db.js
+++ Craving's/server/src/config/db.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 const conectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log("MngoDB connected at ", conn.connect.host);
-    console.log("Database Name :", conn.connect.name);
+    console.log("MngoDB connected at ", conn.connection.host);
+    console.log("Database Name :", conn.connection.name);
   } catch (error) {
     console.log(error);
     process.exit(1);
