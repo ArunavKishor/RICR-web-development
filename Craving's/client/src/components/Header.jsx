c8397d4a98d5eb5513ccd4ee038504d1766416e8
ArunavKishor <arunavkishor@gmail.com>
2026-01-11 17:56:58 +0530

    Refactor code structure for improved readability and maintainability

--- Craving's/client/src/components/Header.jsx
+++ Craving's/client/src/components/Header.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import logotransparent from "../assets/logotransparent.png"
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <>
+    <div className='bg-(--primary) px-4 py-2'>
+        <div className='flex'>
+            <img src={logotransparent} alt="" className='h-12 w-20 object-cover'/>
+            <h1 className=' text-3xl font-bold italic text-white mt-1'>CRAVING'S</h1>
+        </div>
+    </div>
+            
+            
+    </>
   )
 }
 
