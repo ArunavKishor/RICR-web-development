cd0712cf12f4d9cf8ec6b2a0844823b0f5e1d2b2
ArunavKishor <arunavkishor@gmail.com>
2026-01-21 19:25:36 +0530

    nill

--- Craving's/server/index.js
+++ Craving's/server/index.js
@@ -24,6 +24,6 @@ app.use((err, req, res, next) => {
 });
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log("Server started at port:", port);
+  console.log("Server started at port:",port);
   connectDB();
 });
