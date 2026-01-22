6a29749d84a8a0b37d8a0e225f66c3906d0f0d59
ArunavKishor <arunavkishor@gmail.com>
2026-01-22 19:29:36 +0530

    update

--- Craving's/client/src/context/AuthContext.jsx
+++ Craving's/client/src/context/AuthContext.jsx
@@ -5,19 +5,21 @@ import { useState, useEffect } from "react";
 const AuthContext = React.createContext();
 
 export const AuthProvider = (props) => {
-  const [user, setUser] = useState(JSON.parse(sessionStorage.getItem("CravingUser"))||"");//checking backup data 
+  const [user, setUser] = useState(
+    JSON.parse(sessionStorage.getItem("CravingUser")) || "",
+  ); //checking backup data
   const [isLogin, setIsLogin] = useState(!!user);
 
   useEffect(() => {
     setIsLogin(!!user);
   }, [user]);
 
-  const value = { user, setUser, isLogin, setIsLogin };//chanels
+  const value = { user, setUser, isLogin, setIsLogin }; //chanels
   return (
     <AuthContext.Provider value={value}>{props.children}</AuthContext.Provider> //provide to childeren
   );
 };
 
 export const useAuth = () => {
-  return useContext(AuthContext);//sending signal to the users 
+  return useContext(AuthContext); //sending signal to the users
 };
