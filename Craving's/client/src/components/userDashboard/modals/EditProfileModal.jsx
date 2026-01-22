3635aba2a5f00387e13e3442f86e516bd1d4bf06
ArunavKishor <arunavkishor@gmail.com>
2026-01-22 20:41:39 +0530

    update

--- Craving's/client/src/components/userDashboard/modals/EditProfileModal.jsx
+++ Craving's/client/src/components/userDashboard/modals/EditProfileModal.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 
-const EditProfileModal = ({onClose}) => {
+const EditProfileModal = ({ onClose }) => {
   return (
-     <>
-        <div>EditProfileModal</div>
-        <button onClick={()=> onClose()} className="bg-red-400 p-2 px-5 rounded-3xl">X</button>
+    <>
+      <div className="flex fixed inset-0 bg-black/80 items-center justify-center z-100">
+        <div className="bg-white w-5xl max-h-[85vh] overflow-y-auto ">
+          <div>Edit Profile</div>
+          <button
+            onClick={() => onClose()}
+            className="bg-red-400 p-2 px-5 rounded-3xl text-white"
+          >
+            X
+          </button>{" "}
+        </div>
+      </div>
     </>
-  )
+  );
 };
 
 export default EditProfileModal;
