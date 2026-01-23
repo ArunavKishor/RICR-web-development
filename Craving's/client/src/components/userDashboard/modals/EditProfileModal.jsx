62da6073b3ef37b0413c4a089e3c37269fb8ee4e
ArunavKishor <arunavkishor@gmail.com>
2026-01-23 14:07:39 +0530

    update

--- Craving's/client/src/components/userDashboard/modals/EditProfileModal.jsx
+++ Craving's/client/src/components/userDashboard/modals/EditProfileModal.jsx
@@ -26,12 +26,14 @@ const EditProfileModal = ({ onClose }) => {
   return (
     <div className="fixed inset-0 bg-black/80 flex items-center justify-center z-100">
       <div className="bg-white w-5xl max-h-[85vh] overflow-y-auto ">
-        <button
+        <div className="flex justify-end bg-indigo-50">
+          <button
           onClick={onClose}
-          className="bg-red-400 p-2 px-5 rounded-3xl text-white m-4 ml-240"
+          className="bg-red-400 p-2 px-5 rounded-3xl text-white m-4 "
            >
           X
         </button>
+        </div>
 
         <div className="min-h-screen bg-linear-to-br from-blue-50 to-indigo-100 py-6 px-4">
           <div className="max-w-xl mx-auto">
