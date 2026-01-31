d0d6ba9ee1808fff717aaa3c397f321f7c22bb33
ArunavKishor <arunavkishor@gmail.com>
2026-01-31 14:48:51 +0530

    changes

--- Craving's/client/src/components/userDashboard/modals/ResetPasswordModal.jsx
+++ Craving's/client/src/components/userDashboard/modals/ResetPasswordModal.jsx
@@ -4,6 +4,7 @@ import toast from "react-hot-toast";
 import { FaEyeSlash } from "react-icons/fa";
 import { FaEye } from "react-icons/fa";
 
+
 const ResetPasswordModal = ({ onClose }) => {
   const [formData, setFormData] = useState({
     oldPassword: "",
@@ -115,7 +116,7 @@ const ResetPasswordModal = ({ onClose }) => {
                     onClick={() => togglePassword("oldPassword")}
                     className="absolute right-3 top-2.5 text-gray-500 hover:text-gray-700"
                   >
-                    {showPassword.oldPassword ? <FaEyeSlash /> : <FaEye />}
+                    {showPassword.oldPassword ?<FaEyeSlash /> : <FaEye />}
                   </button>
                 </div>
                 {errors.oldPassword && (
